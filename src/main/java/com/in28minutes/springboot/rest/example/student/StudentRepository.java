009c367b50ccefbbab8312a7b4c0d96a1cf4aeb0
debashisCTS <51178137+debashisCTS@users.noreply.github.com>
2025-08-29 16:29:22 +0530

    Update StudentRepository.java

--- src/main/java/com/in28minutes/springboot/rest/example/student/StudentRepository.java
+++ src/main/java/com/in28minutes/springboot/rest/example/student/StudentRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface StudentRepository extends JpaRepository<@NonNull Student, @NonNull Long> {
 //first commit from feature branch
+//second commit from feature branch - after hotfix commit
 }
